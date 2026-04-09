1223a6d4e0473fc7719d245fdb2c2214f99d4710
Brett Ormsby <brettwormsby@outlook.com>
2026-04-08 21:07:31 -0400

    feat: optimize 2016 day 11 using a sorted array

--- src/solutions/2016/day11.ts
+++ src/solutions/2016/day11.ts
@@ -1,6 +1,11 @@
 import type { Solution } from "../../common/index.ts";
 
 /**
+ * * V3 Improvements:
+ * - Move from bits strings for each floor to an array of items with their floor locations
+ * - This allows the items to be sorted so duplicate states where different items are on
+ *   the exact same levels don't both add to the stack
+ *
  * V2 Improvements:
  * - If 2 items of the same type can be brought up, don't try bringing up only 1
  * - If there are no items bellow the current floor, don't try to bring a chip down
@@ -8,54 +13,69 @@ import type { Solution } from "../../common/index.ts";
  *   try one of the pairings
  * */
 
+interface Item {
+  generator: number;
+  chip: number;
+}
+
 export class Day11Year2016 implements Solution {
   // Breadth first algorithm to check all possible situations until
-  // the finishing point is identified. The data is stored as bit strings.
+  // the finishing point is identified. The data is stored as the floor locations
+  // of each item (including the chip and generator).
   first(_input: string): number {
-    // Order is chip (lower order, to right), then generator (higher order, to left)
-    // PO T PR R C
-    const floors = [0b1011101111, 0b0100010000, 0b0000000000, 0b0000000000];
-    return this.computeMinSteps(floors, 5);
+    const items: Item[] = [
+      { generator: 0, chip: 1 },
+      { generator: 0, chip: 0 },
+      { generator: 0, chip: 1 },
+      { generator: 0, chip: 0 },
+      { generator: 0, chip: 0 },
+    ];
+    return this.computeMinSteps(items);
   }
 
   // Do the same as part one with a more complex floor plan
   second(_input: string): number {
-    // E D PO T PR R C
-    const floors = [
-      0b11111011101111, 0b00000100010000, 0b00000000000000, 0b00000000000000,
+    const items: Item[] = [
+      { generator: 0, chip: 0 }, // New
+      { generator: 0, chip: 0 }, // New
+      { generator: 0, chip: 1 },
+      { generator: 0, chip: 0 },
+      { generator: 0, chip: 1 },
+      { generator: 0, chip: 0 },
+      { generator: 0, chip: 0 },
     ];
-    return this.computeMinSteps(floors, 7);
+    return this.computeMinSteps(items);
   }
 
-  private computeMinSteps(floors: number[], bitTypes: number): number {
-    // This is the number when all chips and generators are on the same floor
-    const complete = (1 << (bitTypes * 2)) - 1;
+  private computeMinSteps(items: Item[]): number {
+    const FLOORS = 4;
     const prevStates = new Set<string>();
 
-    const isSafeState = (floors: number[]) => {
-      for (const floor of floors) {
-        let hasIncompleteChip = false;
-        let hasAnyGenerator = false;
-        for (let i = 0; i < bitTypes; i++) {
-          const hasChip = (floor >> (i * 2)) & 1;
-          const hasGenerator = (floor >> (i * 2 + 1)) & 1;
-          if (hasGenerator) hasAnyGenerator = true;
+    const isSafeState = (items: Item[]) => {
+      const hasIncompleteChipArr = new Array(FLOORS).fill(false);
+      const hasAnyGeneratorArr = new Array(FLOORS).fill(false);
 
-          if (hasChip && !hasGenerator) {
-            hasIncompleteChip = true;
-          }
+      for (const { generator, chip } of items) {
+        if (generator !== chip) {
+          hasIncompleteChipArr[chip] = true;
+          hasAnyGeneratorArr[generator] = true;
         }
-        if (hasIncompleteChip && hasAnyGenerator) return false;
+      }
+      for (let i = 0; i < FLOORS; i++) {
+        if (hasAnyGeneratorArr[i] && hasIncompleteChipArr[i]) return false;
       }
       return true;
     };
 
     let steps = 0;
-    let stack = [{ floor: 0, floors }];
-    let nextStack = [];
+    let stack = [{ floor: 0, items }];
+    let nextStack: typeof stack = [];
     while (stack.length) {
-      const { floor, floors } = stack.pop()!;
-      const key = `${floors}|${floor}`;
+      const { floor, items } = stack.pop()!;
+      const sortedItems = items.sort(
+        (a, b) => a.generator - b.generator || a.chip - b.chip,
+      );
+      const key = `${JSON.stringify(sortedItems)}|${floor}`;
       if (prevStates.has(key)) {
         if (stack.length === 0) {
           stack = nextStack;
@@ -66,48 +86,25 @@ export class Day11Year2016 implements Solution {
       }
       prevStates.add(key);
 
-      if (floors[floors.length - 1] === complete) {
+      const isComplete = items.every(
+        ({ generator, chip }) =>
+          generator === FLOORS - 1 && chip === FLOORS - 1,
+      );
+      if (isComplete) {
         return steps;
       }
 
-      const lowestDuplicate = new Map<number, number>();
-      for (let i = 0; i < bitTypes; i++) {
-        lowestDuplicate.set(i, Infinity);
-      }
-      for (let i = 0; i < bitTypes; i++) {
-        let chipPos = -1;
-        let generatorPos = -1;
-        for (let j = 0; j < floors.length; j++) {
-          const hasChip = (floors[j] >> (i * 2)) & 1;
-          if (hasChip) chipPos = j;
-          const hasGenerator = (floors[j] >> (i * 2 + 1)) & 1;
-          if (hasGenerator) generatorPos = j;
-        }
-        for (let j = i + 1; j < bitTypes; j++) {
-          if (lowestDuplicate.get(i)! !== Infinity) continue;
-          const hasSameChip = (floors[chipPos] >> (i * 2)) & 1;
-          const hasSameGenerator = (floors[generatorPos] >> (i * 2 + 1)) & 1;
-          if (hasSameChip && hasSameGenerator) {
-            lowestDuplicate.set(j, i);
-          }
-        }
-      }
-
-      const isOnTopFloor = floor === floors.length - 1;
+      const isOnTopFloor = floor === FLOORS - 1;
 
       // Try moving one chip up and its matching generator
       if (!isOnTopFloor) {
-        for (let i = 0; i < bitTypes; i++) {
-          const hasChip = (floors[floor] >> (i * 2)) & 1;
-          const hasGenerator = (floors[floor] >> (i * 2 + 1)) & 1;
-          if (hasChip && hasGenerator) {
-            const newFloors = [...floors];
-            newFloors[floor] &= ~(1 << (i * 2 + 1)); // Clear Generator
-            newFloors[floor] &= ~(1 << (i * 2)); // Clear Chip
-            newFloors[floor + 1] |= 1 << (i * 2 + 1); // Set Generator
-            newFloors[floor + 1] |= 1 << (i * 2); // Set Chip
+        for (let i = 0; i < items.length; i++) {
+          if (items[i].generator === floor && items[i].chip === floor) {
+            const newItems = [...items.map((element) => ({ ...element }))];
+            newItems[i].generator = floor + 1;
+            newItems[i].chip = floor + 1;
             // Always safe
-            nextStack.push({ floor: floor + 1, floors: newFloors });
+            nextStack.push({ floor: floor + 1, items: newItems });
           }
         }
       }
@@ -116,32 +113,28 @@ export class Day11Year2016 implements Solution {
       if (!isOnTopFloor) {
         let hasBrought2 = false;
         const singles = [];
-        for (let i = 0; i < bitTypes; i++) {
-          const hasGenerator = (floors[floor] >> (i * 2 + 1)) & 1;
+        for (let i = 0; i < items.length; i++) {
+          const hasGenerator = items[i].generator === floor;
           if (!hasGenerator) continue;
 
-          if (!hasBrought2 && i < lowestDuplicate.get(i)!) {
-            const newFloors = [...floors];
-            newFloors[floor] &= ~(1 << (i * 2 + 1)); // Clear Generator
-            newFloors[floor + 1] |= 1 << (i * 2 + 1); // Set Generator
-            if (isSafeState(newFloors)) {
-              singles.push({ floor: floor + 1, floors: newFloors });
+          if (!hasBrought2) {
+            const newItems = [...items.map((element) => ({ ...element }))];
+            newItems[i].generator = floor + 1;
+            if (isSafeState(newItems)) {
+              singles.push({ floor: floor + 1, items: newItems });
             }
           }
 
           // Try moving 2 generators up
-          for (let j = i + 1; j < bitTypes; j++) {
-            const hasGenerator = (floors[floor] >> (j * 2 + 1)) & 1;
+          for (let j = i + 1; j < items.length; j++) {
+            const hasGenerator = items[j].generator === floor;
             if (!hasGenerator) continue;
-
-            const newFloors = [...floors];
-            newFloors[floor] &= ~(1 << (i * 2 + 1)); // Clear Generator
-            newFloors[floor + 1] |= 1 << (i * 2 + 1); // Set Generator
-            newFloors[floor] &= ~(1 << (j * 2 + 1)); // Clear Generator
-            newFloors[floor + 1] |= 1 << (j * 2 + 1); // Set Generator
-            if (isSafeState(newFloors)) {
+            const newItems = [...items.map((element) => ({ ...element }))];
+            newItems[i].generator = floor + 1;
+            newItems[j].generator = floor + 1;
+            if (isSafeState(newItems)) {
               hasBrought2 = true;
-              nextStack.push({ floor: floor + 1, floors: newFloors });
+              nextStack.push({ floor: floor + 1, items: newItems });
             }
           }
         }
@@ -152,32 +145,28 @@ export class Day11Year2016 implements Solution {
       if (!isOnTopFloor) {
         let hasBrought2 = false;
         const singles = [];
-        for (let i = 0; i < bitTypes; i++) {
-          const hasChip = (floors[floor] >> (i * 2)) & 1;
+        for (let i = 0; i < items.length; i++) {
+          const hasChip = items[i].chip === floor;
           if (!hasChip) continue;
 
-          if (!hasBrought2 && i < lowestDuplicate.get(i)!) {
-            const newFloors = [...floors];
-            newFloors[floor] &= ~(1 << (i * 2)); // Clear Chip
-            newFloors[floor + 1] |= 1 << (i * 2); // Set Chip
-            if (isSafeState(newFloors)) {
-              singles.push({ floor: floor + 1, floors: newFloors });
+          if (!hasBrought2) {
+            const newItems = [...items.map((element) => ({ ...element }))];
+            newItems[i].chip = floor + 1;
+            if (isSafeState(newItems)) {
+              singles.push({ floor: floor + 1, items: newItems });
             }
           }
 
           // Try moving 2 chips up
-          for (let j = i + 1; j < bitTypes; j++) {
-            const hasChip = (floors[floor] >> (j * 2)) & 1;
+          for (let j = i + 1; j < items.length; j++) {
+            const hasChip = items[j].chip === floor;
             if (!hasChip) continue;
-
-            const newFloors = [...floors];
-            newFloors[floor] &= ~(1 << (i * 2)); // Clear Chip
-            newFloors[floor + 1] |= 1 << (i * 2); // Set Chip
-            newFloors[floor] &= ~(1 << (j * 2)); // Clear Chip
-            newFloors[floor + 1] |= 1 << (j * 2); // Set Chip
-            if (isSafeState(newFloors)) {
+            const newItems = [...items.map((element) => ({ ...element }))];
+            newItems[i].chip = floor + 1;
+            newItems[j].chip = floor + 1;
+            if (isSafeState(newItems)) {
               hasBrought2 = true;
-              nextStack.push({ floor: floor + 1, floors: newFloors });
+              nextStack.push({ floor: floor + 1, items: newItems });
             }
           }
         }
@@ -186,23 +175,17 @@ export class Day11Year2016 implements Solution {
 
       // Try moving a chip down
       if (floor !== 0) {
-        let allFloorsBellowEmpty = true;
-        for (let i = 0; i < floor; i++) {
-          if (floors[i] !== 0) {
-            allFloorsBellowEmpty = false;
-            break;
-          }
-        }
+        const allFloorsBellowEmpty = items.every(
+          ({ generator, chip }) => generator >= floor && chip >= floor,
+        );
         if (!allFloorsBellowEmpty) {
-          for (let i = 0; i < bitTypes; i++) {
-            const hasChip = (floors[floor] >> (i * 2)) & 1;
-            if (!hasChip) continue;
-
-            const newFloors = [...floors];
-            newFloors[floor] &= ~(1 << (i * 2)); // Clear Chip
-            newFloors[floor - 1] |= 1 << (i * 2); // Set Chip
-            if (isSafeState(newFloors)) {
-              nextStack.push({ floor: floor - 1, floors: newFloors });
+          for (let i = 0; i < items.length; i++) {
+            if (items[i].generator === floor && items[i].chip === floor) {
+              const newItems = [...items.map((element) => ({ ...element }))];
+              newItems[i].chip = floor - 1;
+              if (isSafeState(newItems)) {
+                nextStack.push({ floor: floor - 1, items: newItems });
+              }
             }
           }
         }
@@ -214,7 +197,6 @@ export class Day11Year2016 implements Solution {
         steps += 1;
       }
     }
-
     return -1;
   }
 }
